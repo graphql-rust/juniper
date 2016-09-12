f43be09a95fd59b03d0cdbe8a5905267ea1602e9
Juniper Documentation Builder <nobody@example.com>
2016-09-12 20:19:20 +0000

    Update documentation for Juniper

--- implementors/core/fmt/trait.LowerHex.js
+++ implementors/core/fmt/trait.LowerHex.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];implementors["lazy_static"] = [];implementors["hyper"] = [];
+implementors["lazy_static"] = [];implementors["libc"] = [];implementors["hyper"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
