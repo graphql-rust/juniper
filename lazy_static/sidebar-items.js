549c491c438d80d87683a83b2163fdd9f6526d25
Juniper Documentation Builder <nobody@example.com>
2016-09-20 19:58:37 +0000

    Update documentation for Juniper

--- lazy_static/sidebar-items.js
+++ lazy_static/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"macro":[["lazy_static!",""]]});
+initSidebarItems({"macro":[["__lazy_static_create!",""],["lazy_static!",""]],"mod":[["lazy",""]],"trait":[["__Deref","The `Deref` trait is used to specify the functionality of dereferencing operations, like `*v`."]]});
