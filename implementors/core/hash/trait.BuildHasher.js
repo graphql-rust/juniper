cb596a6b6b09d8b06d4c3a47ec1b987e21ee0554
Juniper Documentation Builder <nobody@example.com>
2016-10-09 12:40:00 +0000

    Update documentation for Juniper

--- implementors/core/hash/trait.BuildHasher.js
+++ implementors/core/hash/trait.BuildHasher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];implementors["lazy_static"] = [];implementors["openssl"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.BuildHasher.html' title='core::hash::BuildHasher'>BuildHasher</a> for <a class='struct' href='https://doc.rust-lang.org/nightly/std/collections/hash/map/struct.RandomState.html' title='std::collections::hash::map::RandomState'>RandomState</a>",];implementors["hyper"] = [];
+implementors["lazy_static"] = [];implementors["libc"] = [];implementors["openssl"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.BuildHasher.html' title='core::hash::BuildHasher'>BuildHasher</a> for <a class='struct' href='https://doc.rust-lang.org/nightly/std/collections/hash/map/struct.RandomState.html' title='std::collections::hash::map::RandomState'>RandomState</a>",];implementors["hyper"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
