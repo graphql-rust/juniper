52957e804a897f5934d630535e34a776b1a526d6
Juniper Documentation Builder <nobody@example.com>
2016-10-09 15:04:17 +0000

    Update documentation for Juniper

--- implementors/core/ops/trait.Shl.js
+++ implementors/core/ops/trait.Shl.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["openssl"] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Shl.html' title='core::ops::Shl'>Shl</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.i32.html'>i32</a>&gt; for &amp;'a <a class='struct' href='openssl/bn/struct.BigNum.html' title='openssl::bn::BigNum'>BigNum</a>",];implementors["hyper"] = [];
+implementors["libc"] = [];implementors["lazy_static"] = [];implementors["openssl"] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Shl.html' title='core::ops::Shl'>Shl</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.i32.html'>i32</a>&gt; for &amp;'a <a class='struct' href='openssl/bn/struct.BigNum.html' title='openssl::bn::BigNum'>BigNum</a>",];implementors["hyper"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
