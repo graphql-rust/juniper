92e0c88fff67511909b20bff977c5638fcbdccb1
Juniper Documentation Builder <nobody@example.com>
2016-10-16 12:23:27 +0000

    Update documentation for Juniper

--- implementors/core/array/trait.FixedSizeArray.js
+++ implementors/core/array/trait.FixedSizeArray.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["hyper"] = [];
+implementors["libc"] = [];implementors["lazy_static"] = [];implementors["hyper"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/hash/trait.Hasher.js
+++ implementors/core/hash/trait.Hasher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["openssl"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html' title='core::hash::Hasher'>Hasher</a> for <a class='struct' href='https://doc.rust-lang.org/nightly/std/collections/hash/map/struct.DefaultHasher.html' title='std::collections::hash::map::DefaultHasher'>DefaultHasher</a>",];implementors["hyper"] = [];
+implementors["libc"] = [];implementors["lazy_static"] = [];implementors["openssl"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html' title='core::hash::Hasher'>Hasher</a> for <a class='struct' href='https://doc.rust-lang.org/nightly/std/collections/hash/map/struct.DefaultHasher.html' title='std::collections::hash::map::DefaultHasher'>DefaultHasher</a>",];implementors["hyper"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
