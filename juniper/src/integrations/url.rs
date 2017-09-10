452d2f65d3dd24894b40d0eb689cd2e54fc6dd3c
Christian Legnitto <christian@legnitto.com>
2017-09-10 12:56:39 -0700

    url feature: Implement url::Url as a scalar type

---
+++ juniper/src/integrations/url.rs
@@ -0,0 +1,32 @@
+use url::Url;
+
+use ::Value;
+
+graphql_scalar!(Url {
+    description: "Url"
+
+    resolve(&self) -> Value {
+        Value::string(self.as_str())
+    }
+
+    from_input_value(v: &InputValue) -> Option<Url> {
+        v.as_string_value()
+         .and_then(|s| Url::parse(s).ok())
+    }
+});
+
+#[cfg(test)]
+mod test {
+    use url::Url;
+
+    #[test]
+    fn url_from_input_value() {
+        let raw = "https://example.net/";
+        let input = ::InputValue::String(raw.to_string());
+
+        let parsed: Url = ::FromInputValue::from_input_value(&input).unwrap();
+        let url = Url::parse(raw).unwrap();
+
+        assert_eq!(parsed, url);
+    }
+}
