2a2534525d6d6684e1e48d592b8a013199e80b5e
Christian Legnitto <christian@legnitto.com>
2017-09-26 22:53:16 +0300

    chrono feature: Implement GraphQLType for chrono types
    Fixes https://github.com/graphql-rust/juniper/issues/87

--- juniper/src/integrations/mod.rs
+++ juniper/src/integrations/mod.rs
@@ -1,7 +1,14 @@
+#[doc(hidden)]
 pub mod serde;
 
+#[cfg(feature = "chrono")]
+/// GraphQL support for [chrono](https://github.com/chronotope/chrono) types.
+pub mod chrono;
+
 #[cfg(feature = "url")]
-mod url;
+/// GraphQL support for [url](https://github.com/servo/rust-url) types.
+pub mod url;
 
 #[cfg(feature = "uuid")]
-mod uuid;
+/// GraphQL support for [uuid](https://doc.rust-lang.org/uuid/uuid/struct.Uuid.html) types.
+pub mod uuid;
