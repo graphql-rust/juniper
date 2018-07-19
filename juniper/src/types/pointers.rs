4dc155c51a3c437e803eda0b7d2d7e9932208521
Paul Colomiets <paul@colomiets.name>
2018-07-19 13:47:09 +0300

    Implement GraphQLType and ToInputValue for Arc<T>

--- juniper/src/types/pointers.rs
+++ juniper/src/types/pointers.rs
@@ -1,3 +1,4 @@
+use std::sync::Arc;
 use ast::{FromInputValue, InputValue, Selection, ToInputValue};
 use value::Value;
 
@@ -117,6 +118,60 @@ where
 }
 
 impl<'a, T> ToInputValue for &'a T
+where
+    T: ToInputValue,
+{
+    fn to_input_value(&self) -> InputValue {
+        (**self).to_input_value()
+    }
+}
+
+impl<T, CtxT> GraphQLType for Arc<T>
+where
+    T: GraphQLType<Context = CtxT>,
+{
+    type Context = CtxT;
+    type TypeInfo = T::TypeInfo;
+
+    fn name(info: &T::TypeInfo) -> Option<&str> {
+        T::name(info)
+    }
+
+    fn meta<'r>(info: &T::TypeInfo, registry: &mut Registry<'r>) -> MetaType<'r> {
+        T::meta(info, registry)
+    }
+
+    fn resolve_into_type(
+        &self,
+        info: &T::TypeInfo,
+        name: &str,
+        selection_set: Option<&[Selection]>,
+        executor: &Executor<CtxT>,
+    ) -> ExecutionResult {
+        (**self).resolve_into_type(info, name, selection_set, executor)
+    }
+
+    fn resolve_field(
+        &self,
+        info: &T::TypeInfo,
+        field: &str,
+        args: &Arguments,
+        executor: &Executor<CtxT>,
+    ) -> ExecutionResult {
+        (**self).resolve_field(info, field, args, executor)
+    }
+
+    fn resolve(
+        &self,
+        info: &T::TypeInfo,
+        selection_set: Option<&[Selection]>,
+        executor: &Executor<CtxT>,
+    ) -> Value {
+        (**self).resolve(info, selection_set, executor)
+    }
+}
+
+impl<T> ToInputValue for Arc<T>
 where
     T: ToInputValue,
 {
