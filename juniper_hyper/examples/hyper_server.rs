7826d6b1c085728936a43c42ae6d9664d3ae0a14
Damir <damir@qdentity.nl>
2018-08-12 21:16:43 +0200

    Add a WIP v2

--- juniper_hyper/examples/hyper_server.rs
+++ juniper_hyper/examples/hyper_server.rs
@@ -32,32 +32,33 @@ fn main() {
     let pool = CpuPool::new(4);
     let db = Arc::new(Database::new());
 
-    let new_service = move || {
-        let pool = pool.clone();
-        let db = db.clone();
-        service_fn(move |req| match (req.method(), req.uri().path()) {
-            (&Method::GET, "/hello") => {
-                let mut response = Response::new(Body::empty());
-                *response.body_mut() = Body::from("Try POSTing data to /graphql");
-                Box::new(future::ok(response))
-            }
-            (&Method::POST, "/graphql") => {
-                let mut service = GraphQLHandler::new(
-                    pool.clone(),
-                    context_factory,
-                    db.clone(),
-                    EmptyMutation::<Database>::new(),
-                );
-                service.call(req)
-            }
-            _ => {
-                let mut response = Response::new(Body::empty());
-                *response.status_mut() = StatusCode::NOT_FOUND;
-                Box::new(future::ok(response))
-            }
-        })
-    };
-
+    //    let new_service = move || {
+    //        let pool = pool.clone();
+    //        let db = db.clone();
+    //        service_fn(move |req| match (req.method(), req.uri().path()) {
+    //            (&Method::GET, "/hello") => {
+    //                let mut response = Response::new(Body::empty());
+    //                *response.body_mut() = Body::from("Try POSTing data to /graphql");
+    //                Box::new(future::ok(response))
+    //            }
+    //            (&Method::POST, "/graphql") => {
+    //                let mut service = GraphQLHandler::new(
+    //                    pool.clone(),
+    //                    context_factory,
+    //                    db.clone(),
+    //                    EmptyMutation::<Database>::new(),
+    //                );
+    //                service.call(req)
+    //            }
+    //            _ => {
+    //                let mut response = Response::new(Body::empty());
+    //                *response.status_mut() = StatusCode::NOT_FOUND;
+    //                Box::new(future::ok(response))
+    //            }
+    //        })
+    //    };
+    let new_service =
+        move || Ok(MyService { pool: pool.clone() }) as Result<MyService, Box<hyper::Error>>;
     let server = Server::bind(&addr)
         .serve(new_service)
         .map_err(|e| eprintln!("server error: {}", e));
--- juniper_hyper/src/lib.rs
+++ juniper_hyper/src/lib.rs
@@ -2,76 +2,214 @@ extern crate futures;
 extern crate futures_cpupool;
 extern crate hyper;
 extern crate juniper;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate url;
 
-use futures_cpupool::{CpuFuture, CpuPool};
-
-use futures::future::IntoFuture;
-use futures::Future;
-use hyper::service::{NewService, Service};
-use hyper::Request;
-use hyper::{Body, Response, Server};
-use juniper::GraphQLType;
-use juniper::RootNode;
-
-use hyper::body::Payload;
+use futures::future;
+use futures::{Future, Stream};
+use futures_cpupool::CpuPool;
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use juniper::http::{
+    GraphQLRequest as JuniperGraphQLRequest, GraphQLResponse as JuniperGraphQLResponse,
+};
+use juniper::{GraphQLType, InputValue, RootNode};
+use serde_json::error::Error as SerdeError;
 use std::error::Error as StdError;
+use std::error::Error;
+use std::fmt;
+use std::string::FromUtf8Error;
+use url::form_urlencoded;
 
-pub struct GraphQLHandler<'a, CtxFactory, CtxRes, Query, Mutation, CtxT>
+pub fn handle_graphql<CtxT, QueryT, MutationT>(
+    pool: &CpuPool,
+    root_node: &RootNode<QueryT, MutationT>,
+    context: &CtxT,
+    request: Request<Body>,
+) -> Box<Future<Item = Response<Body>, Error = ()>>
 where
-    CtxFactory: Fn(&mut Request<Body>) -> CtxRes + Send + Sync,
-    CtxRes: IntoFuture<Item = CtxT>,
-    CtxRes::Error: Into<Box<StdError + Send + Sync>>,
-    Query: GraphQLType<Context = CtxT> + Send + Sync,
-    Mutation: GraphQLType<Context = CtxT> + Send + Sync,
+    QueryT: GraphQLType<Context = CtxT>,
+    MutationT: GraphQLType<Context = CtxT>,
 {
-    pool: CpuPool,
-    context_factory: CtxFactory,
-    root_node: RootNode<'a, Query, Mutation>,
-}
-
-impl<'a, CtxFactory, CtxRes, Query, Mutation, CtxT>
-    GraphQLHandler<'a, CtxFactory, CtxRes, Query, Mutation, CtxT>
-where
-    CtxFactory: Fn(&mut Request<Body>) -> CtxRes + Send + Sync,
-    CtxRes: IntoFuture<Item = CtxT>,
-    CtxRes::Error: Into<Box<StdError + Send + Sync>>,
-    Query: GraphQLType<Context = CtxT, TypeInfo = ()> + Send + Sync,
-    Mutation: GraphQLType<Context = CtxT, TypeInfo = ()> + Send + Sync,
-{
-    pub fn new(
-        pool: CpuPool,
-        context_factory: CtxFactory,
-        root_node: RootNode<'a, Query, Mutation>,
-    ) -> Self {
-        GraphQLHandler::new_with_info(pool, context_factory, query, (), mutation, ())
+    match request.method() {
+        &Method::GET => Box::new(
+            future::ok(
+                request
+                    .uri()
+                    .query()
+                    .map(gql_request_from_get)
+                    .unwrap_or(Err(GraphQLRequestError::Invalid(
+                        "'query' parameter is missing".to_string(),
+                    ))),
+            ).map(|res| new_response(StatusCode::METHOD_NOT_ALLOWED)),
+        ),
+        &Method::POST => Box::new(
+            request
+                .into_body()
+                .concat2()
+                .map(move |chunk| {
+                    String::from_utf8(chunk.iter().rev().cloned().collect::<Vec<u8>>())
+                        .map_err(GraphQLRequestError::BodyUtf8)
+                        .and_then(|input| {
+                            serde_json::from_str::<GraphQLRequest>(&input)
+                                .map_err(GraphQLRequestError::BodyJSONError)
+                        })
+                }).or_else(|err| future::ok(Err(GraphQLRequestError::BodyHyper(err))))
+                .map(|res| new_response(StatusCode::METHOD_NOT_ALLOWED)),
+        ),
+        _ => return Box::new(future::ok(new_response(StatusCode::METHOD_NOT_ALLOWED))),
     }
 }
 
-impl<'a, CtxFactory, CtxRes, Query, Mutation, CtxT> Service
-    for GraphQLHandler<'a, CtxFactory, CtxRes, Query, Mutation, CtxT>
-where
-    CtxT: Send,
-    CtxFactory: Fn(&mut Request<Body>) -> CtxRes + Send + Sync,
-    CtxRes: IntoFuture<Item = CtxT> + Send,
-    CtxRes::Error: Into<Box<StdError + Send + Sync>>,
-    CtxRes::Future: 'static + Send,
-    Query: GraphQLType<Context = CtxT> + Send + Sync,
-    Mutation: GraphQLType<Context = CtxT> + Send + Sync,
-{
-    type ReqBody = Body;
-    type ResBody = Body;
-    type Error = Box<std::error::Error + Send + Sync>;
-    type Future = Box<Future<Item = Response<Self::ResBody>, Error = Self::Error> + Send>;
+fn gql_request_from_get(input: &str) -> Result<JuniperGraphQLRequest, GraphQLRequestError> {
+    let mut query = None;
+    let mut operation_name = None;
+    let mut variables = None;
+    for (key, value) in form_urlencoded::parse(input.as_bytes()).into_owned() {
+        match key.as_ref() {
+            "query" => {
+                if query.is_some() {
+                    return Err(invalid_err("query"));
+                }
+                query = Some(value)
+            }
+            "operationName" => {
+                if operation_name.is_some() {
+                    return Err(invalid_err("operationName"));
+                }
+            }
+            "variables" => {
+                if variables.is_some() {
+                    return Err(invalid_err("variables"));
+                }
+                match serde_json::from_str::<InputValue>(&value)
+                    .map_err(GraphQLRequestError::Variables)
+                {
+                    Ok(parsed_variables) => variables = Some(parsed_variables),
+                    Err(e) => return Err(e),
+                }
+            }
+            _ => continue,
+        }
+    }
+    match query {
+        Some(query) => Ok(JuniperGraphQLRequest::new(query, operation_name, variables)),
+        None => Err(GraphQLRequestError::Invalid(
+            "'query' parameter is missing".to_string(),
+        )),
+    }
+}
 
-    fn call(&mut self, mut req: Request<Body>) -> Self::Future {
-        Box::new(
-            (self.context_factory)(&mut req)
-                .into_future()
-                .map_err(|err| err.into())
-                .map(|res: CtxT| Response::new(Body::from("YO!"))),
-        )
+fn invalid_err(parameter_name: &str) -> GraphQLRequestError {
+    GraphQLRequestError::Invalid(format!(
+        "'{}' parameter is specified multiple times",
+        parameter_name
+    ))
+}
+
+fn new_response(code: StatusCode) -> Response<Body> {
+    let mut r = Response::new(Body::empty());
+    *r.status_mut() = code;
+    r
+}
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+enum GraphQLRequest {
+    Single(JuniperGraphQLRequest),
+    Batch(Vec<JuniperGraphQLRequest>),
+}
+
+impl GraphQLRequest {
+    pub fn execute<'a, CtxT, QueryT, MutationT>(
+        &'a self,
+        root_node: &RootNode<QueryT, MutationT>,
+        context: &CtxT,
+    ) -> GraphQLResponse<'a>
+    where
+        QueryT: GraphQLType<Context = CtxT>,
+        MutationT: GraphQLType<Context = CtxT>,
+    {
+        match self {
+            &GraphQLRequest::Single(ref request) => {
+                GraphQLResponse::Single(request.execute(root_node, context))
+            }
+            &GraphQLRequest::Batch(ref requests) => GraphQLResponse::Batch(
+                requests
+                    .iter()
+                    .map(|request| request.execute(root_node, context))
+                    .collect(),
+            ),
+        }
+    }
+}
+
+#[derive(Serialize)]
+#[serde(untagged)]
+enum GraphQLResponse<'a> {
+    Single(JuniperGraphQLResponse<'a>),
+    Batch(Vec<JuniperGraphQLResponse<'a>>),
+}
+
+impl<'a> GraphQLResponse<'a> {
+    fn is_ok(&self) -> bool {
+        match self {
+            &GraphQLResponse::Single(ref response) => response.is_ok(),
+            &GraphQLResponse::Batch(ref responses) => responses
+                .iter()
+                .fold(true, |ok, response| ok && response.is_ok()),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum GraphQLRequestError {
+    BodyHyper(hyper::Error),
+    BodyUtf8(FromUtf8Error),
+    BodyJSONError(SerdeError),
+    Variables(SerdeError),
+    Invalid(String),
+}
+
+impl fmt::Display for GraphQLRequestError {
+    fn fmt(&self, mut f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            GraphQLRequestError::BodyHyper(ref err) => fmt::Display::fmt(err, &mut f),
+            GraphQLRequestError::BodyUtf8(ref err) => fmt::Display::fmt(err, &mut f),
+            GraphQLRequestError::BodyJSONError(ref err) => fmt::Display::fmt(err, &mut f),
+            GraphQLRequestError::Variables(ref err) => fmt::Display::fmt(err, &mut f),
+            GraphQLRequestError::Invalid(ref err) => fmt::Display::fmt(err, &mut f),
+        }
+    }
+}
+
+impl Error for GraphQLRequestError {
+    fn description(&self) -> &str {
+        match *self {
+            GraphQLRequestError::BodyHyper(ref err) => err.description(),
+            GraphQLRequestError::BodyUtf8(ref err) => err.description(),
+            GraphQLRequestError::BodyJSONError(ref err) => err.description(),
+            GraphQLRequestError::Variables(ref err) => err.description(),
+            GraphQLRequestError::Invalid(ref err) => err,
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            GraphQLRequestError::BodyHyper(ref err) => Some(err),
+            GraphQLRequestError::BodyUtf8(ref err) => Some(err),
+            GraphQLRequestError::BodyJSONError(ref err) => Some(err),
+            GraphQLRequestError::Variables(ref err) => Some(err),
+            GraphQLRequestError::Invalid(_) => None,
+        }
     }
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    #[test]
+    fn parse_query_as_graphql_request() {
+        assert_eq!(2 + 2, 4);
+    }
+}
