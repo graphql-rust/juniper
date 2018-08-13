ca3df5579a2147bdf666175882709cba02eb90d5
Damir <damir@qdentity.nl>
2018-08-13 12:27:58 +0200

    Cleanup

--- juniper_hyper/examples/hyper_server.rs
+++ juniper_hyper/examples/hyper_server.rs
@@ -8,21 +8,13 @@ extern crate pretty_env_logger;
 use futures::future;
 use futures_cpupool::CpuPool;
 use hyper::rt::{self, Future};
-use hyper::service::{service_fn, Service};
-use hyper::{Body, Request, Response, Server, StatusCode};
+use hyper::service::service_fn;
+use hyper::Method;
+use hyper::{Body, Response, Server, StatusCode};
 use juniper::tests::model::Database;
 use juniper::EmptyMutation;
-
-use hyper::Method;
 use juniper::RootNode;
-use std::error::Error as StdError;
 use std::sync::Arc;
-
-fn context_factory(_: &mut Request<Body>) -> Result<Database, Box<StdError + Send + Sync>> {
-    Ok(Database::new())
-}
-
-static PHRASE: &'static [u8] = b"Hello World!";
 
 fn main() {
     pretty_env_logger::init();
@@ -32,26 +24,18 @@ fn main() {
     let pool = CpuPool::new(4);
     let db = Arc::new(Database::new());
     let root_node = Arc::new(RootNode::new(db.clone(), EmptyMutation::<Database>::new()));
-    let ctx = Arc::new(Database::new());
 
     let new_service = move || {
         let pool = pool.clone();
         let root_node = root_node.clone();
-        let ctx = ctx.clone();
+        let ctx = db.clone();
         service_fn(move |req| {
             let pool = pool.clone();
             let root_node = root_node.clone();
             let ctx = ctx.clone();
             match (req.method(), req.uri().path()) {
-                (&Method::GET, "/") => juniper_hyper::handle_playground("/graphql"),
-                (&Method::GET, "/hello") => {
-                    let mut response = Response::new(Body::empty());
-                    *response.body_mut() = Body::from("Try POSTing data to /graphql");
-                    Box::new(future::ok(response))
-                }
-                (&Method::POST, "/graphql") => {
-                    juniper_hyper::handle_graphql(pool, root_node, ctx, req)
-                }
+                (&Method::GET, "/") => juniper_hyper::playground("/graphql"),
+                (&Method::POST, "/graphql") => juniper_hyper::graphql(pool, root_node, ctx, req),
                 _ => {
                     let mut response = Response::new(Body::empty());
                     *response.status_mut() = StatusCode::NOT_FOUND;
@@ -60,8 +44,6 @@ fn main() {
             }
         })
     };
-    //    let new_service =
-    //        move || Ok(MyService { pool: pool.clone() }) as Result<MyService, Box<hyper::Error>>;
     let server = Server::bind(&addr)
         .serve(new_service)
         .map_err(|e| eprintln!("server error: {}", e));
--- juniper_hyper/src/lib.rs
+++ juniper_hyper/src/lib.rs
@@ -24,49 +24,7 @@ use std::string::FromUtf8Error;
 use std::sync::Arc;
 use url::form_urlencoded;
 
-pub fn handle_playground(
-    graphql_endpoint: &str,
-) -> Box<Future<Item = Response<Body>, Error = hyper::Error> + Send> {
-    let mut resp = new_response(StatusCode::OK);
-    resp.headers_mut()
-        .insert(header::CONTENT_TYPE, HeaderValue::from_static("text/html"));
-    *resp.body_mut() = Body::from(format!(r#"<!DOCTYPE html>
-<html>
-<head>
-	<meta charset=utf-8/>
-	<meta name="viewport" content="user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
-	<link rel="shortcut icon" href="https://graphcool-playground.netlify.com/favicon.png">
-	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/graphql-playground-react@{version}/build/static/css/index.css"/>
-	<link rel="shortcut icon" href="//cdn.jsdelivr.net/npm/graphql-playground-react@{version}/build/favicon.png"/>
-	<script src="//cdn.jsdelivr.net/npm/graphql-playground-react@{version}/build/static/js/middleware.js"></script>
-	<title>GraphQL Playground</title>
-</head>
-<body>
-<style type="text/css">
-	html {{ font-family: "Open Sans", sans-serif; overflow: hidden; }}
-	body {{ margin: 0; background: #172a3a; }}
-</style>
-<div id="root"/>
-<script type="text/javascript">
-	window.addEventListener('load', function (event) {{
-		const root = document.getElementById('root');
-		root.classList.add('playgroundIn');
-		const wsProto = location.protocol == 'https:' ? 'wss:' : 'ws:'
-		GraphQLPlayground.init(root, {{
-			endpoint: location.protocol + '//' + location.host + '{graphql_url}',
-			subscriptionsEndpoint: wsProto + '//' + location.host + '{graphql_url}',
-			settings: {{
-				'request.credentials': 'same-origin'
-			}}
-		}})
-	}})
-</script>
-</body>
-</html>"#, version = "1.6.2", graphql_url = graphql_endpoint));
-    Box::new(future::ok(resp))
-}
-
-pub fn handle_graphql<CtxT, QueryT, MutationT>(
+pub fn graphql<CtxT, QueryT, MutationT>(
     pool: CpuPool,
     root_node: Arc<RootNode<'static, QueryT, MutationT>>,
     context: Arc<CtxT>,
@@ -111,6 +69,48 @@ where
         ),
         _ => return Box::new(future::ok(new_response(StatusCode::METHOD_NOT_ALLOWED))),
     }
+}
+
+pub fn playground(
+    graphql_endpoint: &str,
+) -> Box<Future<Item = Response<Body>, Error = hyper::Error> + Send> {
+    let mut resp = new_response(StatusCode::OK);
+    resp.headers_mut()
+        .insert(header::CONTENT_TYPE, HeaderValue::from_static("text/html"));
+    *resp.body_mut() = Body::from(format!(r#"<!DOCTYPE html>
+<html>
+<head>
+	<meta charset=utf-8/>
+	<meta name="viewport" content="user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
+	<link rel="shortcut icon" href="https://graphcool-playground.netlify.com/favicon.png">
+	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/graphql-playground-react@{version}/build/static/css/index.css"/>
+	<link rel="shortcut icon" href="//cdn.jsdelivr.net/npm/graphql-playground-react@{version}/build/favicon.png"/>
+	<script src="//cdn.jsdelivr.net/npm/graphql-playground-react@{version}/build/static/js/middleware.js"></script>
+	<title>GraphQL Playground</title>
+</head>
+<body>
+<style type="text/css">
+	html {{ font-family: "Open Sans", sans-serif; overflow: hidden; }}
+	body {{ margin: 0; background: #172a3a; }}
+</style>
+<div id="root"/>
+<script type="text/javascript">
+	window.addEventListener('load', function (event) {{
+		const root = document.getElementById('root');
+		root.classList.add('playgroundIn');
+		const wsProto = location.protocol == 'https:' ? 'wss:' : 'ws:'
+		GraphQLPlayground.init(root, {{
+			endpoint: location.protocol + '//' + location.host + '{graphql_url}',
+			subscriptionsEndpoint: wsProto + '//' + location.host + '{graphql_url}',
+			settings: {{
+				'request.credentials': 'same-origin'
+			}}
+		}})
+	}})
+</script>
+</body>
+</html>"#, version = "1.6.2", graphql_url = graphql_endpoint));
+    Box::new(future::ok(resp))
 }
 
 fn render_error(err: GraphQLRequestError) -> Response<Body> {
