d496220e1072504f25fd4bd4d423f993a92390c5
Yusuke Sasaki <ubnt-intrepid@users.noreply.github.com>
2018-08-27 04:11:27 +0900

    Use `local_inner_macros` to all helper macros (#233)
    This enables  Rust2018-style macro imports:
    
    ```rust
    use juniper::graphql_object;
    
    graphql_object!(User: () |&self| { ... });
    ```
    
    In the future when dropping compatibility with pre-2018 compilers,
    this can be replaced with the explicit `$crate` prefixes instead of
    `local_inner_macros`.
    
    In `macro_rules!` with the annotation `local_inner_macros`, all of macro calls
    are transformed into `$crate::builtin!(...)` (See [1] for details).
    Therefore, name resolutions of built-in macros are not perfomed correctly.
    To avoid this, some helper macros are introduced to make built-in
    macros be interpreted as crate-local macros.
    
    [1]: https://github.com/rust-lang/rust/issues/53464#issuecomment-414049821

--- juniper/src/macros/mod.rs
+++ juniper/src/macros/mod.rs
@@ -1,3 +1,23 @@
+// Wrapper macros which allows built-in macros to be recognized as "crate-local".
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __graphql__panic {
+    ($($t:tt)*) => ( panic!($($t)*) );
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __graphql__stringify {
+    ($($t:tt)*) => ( stringify!($($t)*) );
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __graphql__vec {
+    ($($t:tt)*) => ( vec!($($t)*) );
+}
+
 #[macro_use]
 mod object;
 #[macro_use]
