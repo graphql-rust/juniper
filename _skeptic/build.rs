1e60fab28aa1e0fb65295207a44f0cfcc964962f
Christian Legnitto <christian@legnitto.com>
2019-01-14 16:20:00 -0800

    Rename "docs" to "src"

--- _skeptic/build.rs
+++ _skeptic/build.rs
@@ -1,6 +1,6 @@
 extern crate skeptic;
 
 fn main() {
-    let files = skeptic::markdown_files_of_directory("../docs/");
+    let files = skeptic::markdown_files_of_directory("../src/");
     skeptic::generate_doc_tests(&files);
 }
