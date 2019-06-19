3e5eada4c68db958bc4d890177850c0bcda1db8e
Christian Legnitto <christian@legnitto.com>
2019-06-18 23:02:17 -0400

    Make `.cargo/bin` if it does not exist

--- _build/cargo-make.sh
+++ _build/cargo-make.sh
@@ -14,5 +14,6 @@ fi
 
 curl https://github.com/sagiegurari/cargo-make/releases/download/${1}/cargo-make-v${1}-${2}.zip -sSfL -o /tmp/cargo-make.zip;
 unzip /tmp/cargo-make.zip;
+mkdir -p "$HOME/.cargo/bin";
 mv cargo-make-*/* $HOME/.cargo/bin;
 echo "##vso[task.setvariable variable=PATH;]$PATH:$HOME/.cargo/bin"
