4016d38d3de840d6a82bf9443fc1b01fbb295095
Christian Legnitto <christian.legnitto@robinhood.com>
2019-07-24 16:17:20 -0600

    Don't assume lookaheads are always fields
    Fixes https://github.com/graphql-rust/juniper/issues/398.

---
+++ integration_tests/juniper_tests/src/issue_398.rs
@@ -0,0 +1,67 @@
+// Original author of this test is <https://github.com/davidpdrsn>.
+use juniper::*;
+
+struct Query;
+
+#[juniper::object]
+impl Query {
+    fn users(executor: &Executor) -> Vec<User> {
+        // This doesn't cause a panic
+        executor.look_ahead();
+
+        vec![User {
+            country: Country { id: 1 },
+        }]
+    }
+}
+
+struct User {
+    country: Country,
+}
+
+#[juniper::object]
+impl User {
+    fn country(&self, executor: &Executor) -> &Country {
+        // This panics!
+        executor.look_ahead();
+
+        &self.country
+    }
+}
+
+struct Country {
+    id: i32,
+}
+
+#[juniper::object]
+impl Country {
+    fn id(&self) -> i32 {
+        self.id
+    }
+}
+
+type Schema = juniper::RootNode<'static, Query, EmptyMutation<()>>;
+
+#[test]
+fn it_works() {
+    let _ = juniper::execute(
+        r#"
+            query Query {
+                users {
+                    ...userFields
+                }
+            }
+
+            fragment userFields on User {
+                country {
+                    id
+                }
+            }
+        "#,
+        None,
+        &Schema::new(Query, EmptyMutation::new()),
+        &Variables::new(),
+        &(),
+    )
+    .unwrap();
+}
