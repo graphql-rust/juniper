644d6042aebfbfe79b416f7009c10d5676ee75f4
Christian Legnitto <christian.legnitto@robinhood.com>
2019-07-29 08:06:22 -0700

    Release juniper_codegen 0.13.1

--- juniper_codegen/src/lib.rs
+++ juniper_codegen/src/lib.rs
@@ -4,7 +4,7 @@
 //! You should not depend on juniper_codegen directly.
 //! You only need the `juniper` crate.
 
-#![doc(html_root_url = "https://docs.rs/juniper_codegen/0.13.0")]
+#![doc(html_root_url = "https://docs.rs/juniper_codegen/0.13.1")]
 #![recursion_limit = "1024"]
 
 extern crate proc_macro;
