5185eb3758a1e9ea5104a94e97ba57e4fb1d8308
nWacky <gosha.evtushenko@gmail.com>
2019-09-24 17:01:33 +0300

    Add subscription type resolvers to rocket server example (testing only)

--- juniper_rocket/examples/rocket_server.rs
+++ juniper_rocket/examples/rocket_server.rs
@@ -1,13 +1,49 @@
+// This file is used for testing implementantions
+//_!! All changes should be reset before merging to master !!__
+
 #![feature(decl_macro, proc_macro_hygiene)]
 
 use rocket::{response::content, State};
 
-use juniper::{
-    tests::{model::Database, schema::Query},
-    EmptyMutation, RootNode,
-};
+use juniper::{RootNode, FieldResult};
 
-type Schema = RootNode<'static, Query, EmptyMutation<Database>, EmptyMutation<Database>>;
+#[derive(juniper::GraphQLObject)]
+#[graphql(description = "A humanoid creature in the Star Wars universe")]
+struct Human {
+    id: String,
+    name: String,
+    home_planet: String,
+}
+
+struct MyMutation;
+
+#[juniper::object]
+impl MyMutation {
+    fn human(id: String) -> FieldResult<Human> {
+        let human = Human {
+            id: "123".to_string(),
+            name: "Human Name".to_string(),
+            home_planet: "Human Home Planet".to_string(),
+        };
+        Ok(human)
+    }
+}
+
+struct MyQuery;
+
+#[juniper::object]
+impl MyQuery {
+    fn human(id: String) -> FieldResult<Human> {
+        let human = Human {
+            id: "000".to_string(),
+            name: "Query Human".to_string(),
+            home_planet: "Query Human Planet".to_string(),
+        };
+        Ok(human)
+    }
+}
+
+type Schema = RootNode<'static, MyQuery, MyMutation, MyMutation>;
 
 #[rocket::get("/")]
 fn graphiql() -> content::Html<String> {
@@ -16,26 +52,23 @@ fn graphiql() -> content::Html<String> {
 
 #[rocket::get("/graphql?<request>")]
 fn get_graphql_handler(
-    context: State<Database>,
     request: juniper_rocket::GraphQLRequest,
     schema: State<Schema>,
 ) -> juniper_rocket::GraphQLResponse {
-    request.execute(&schema, &context)
+    request.execute(&schema, &())
 }
 
 #[rocket::post("/graphql", data = "<request>")]
 fn post_graphql_handler(
-    context: State<Database>,
     request: juniper_rocket::GraphQLRequest,
     schema: State<Schema>,
 ) -> juniper_rocket::GraphQLResponse {
-    request.execute(&schema, &context)
+    request.execute(&schema, &())
 }
 
 fn main() {
     rocket::ignite()
-        .manage(Database::new())
-        .manage(Schema::new(Query, EmptyMutation::<Database>::new(), EmptyMutation::<Database>::new()))
+        .manage(Schema::new(MyQuery, MyMutation, MyMutation))
         .mount(
             "/",
             rocket::routes![graphiql, get_graphql_handler, post_graphql_handler],
