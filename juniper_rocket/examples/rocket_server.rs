3eab17caef84e602a5239dd2e47c58de46de0c4b
nWacky <gosha.evtushenko@gmail.com>
2019-09-24 16:38:42 +0300

    Add subscription type resolvers [may work]

--- juniper_rocket/examples/rocket_server.rs
+++ juniper_rocket/examples/rocket_server.rs
@@ -7,7 +7,7 @@ use juniper::{
     EmptyMutation, RootNode,
 };
 
-type Schema = RootNode<'static, Query, EmptyMutation<Database>>;
+type Schema = RootNode<'static, Query, EmptyMutation<Database>, EmptyMutation<Database>>;
 
 #[rocket::get("/")]
 fn graphiql() -> content::Html<String> {
@@ -35,7 +35,7 @@ fn post_graphql_handler(
 fn main() {
     rocket::ignite()
         .manage(Database::new())
-        .manage(Schema::new(Query, EmptyMutation::<Database>::new()))
+        .manage(Schema::new(Query, EmptyMutation::<Database>::new(), EmptyMutation::<Database>::new()))
         .mount(
             "/",
             rocket::routes![graphiql, get_graphql_handler, post_graphql_handler],
