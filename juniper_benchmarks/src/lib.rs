6aebe5a30fcc5d79f5dfbc79894e426f77784c04
Christian Legnitto <christian.legnitto@robinhood.com>
2020-01-20 23:23:42 -0800

    Add features to juniper_benchmarks

--- juniper_benchmarks/src/lib.rs
+++ juniper_benchmarks/src/lib.rs
@@ -105,6 +105,7 @@ pub fn execute(query: &str, vars: Variables) -> QueryResult {
     juniper::execute(query, None, &root, &vars, &ctx).map_err(|e| format!("{:?}", e))
 }
 
+#[cfg(feature = "async")]
 pub async fn execute_async(query: &str, vars: Variables) -> QueryResult {
     let root = new_schema();
     let ctx = Context::new();
