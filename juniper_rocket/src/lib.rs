8839552060c10094b0f9a33b5e26d58866364750
Christian Legnitto <christian.legnitto@robinhood.com>
2020-01-29 02:50:28 -0500

    Fix test

--- juniper_rocket/src/lib.rs
+++ juniper_rocket/src/lib.rs
@@ -39,10 +39,7 @@ Check the LICENSE file for details.
 #![doc(html_root_url = "https://docs.rs/juniper_rocket/0.2.0")]
 #![feature(decl_macro, proc_macro_hygiene)]
 
-use std::{
-    error::Error,
-    io::{Cursor, Read},
-};
+use std::io::{Cursor, Read};
 
 use rocket::{
     data::{FromDataSimple, Outcome as FromDataOutcome},
@@ -425,7 +422,7 @@ mod fromform_tests {
 
     #[test]
     fn test_variables_invalid_json() {
-        check_error("query=test&variables=NOT_JSON", "JSON error", false);
+        check_error("query=test&variables=NOT_JSON", "expected value at line 1 column 1", false);
     }
 
     #[test]
