619fe15605e893021df3694df6ddacab5ab863a1
Jordão <jordao.rosario01@gmail.com>
2020-02-03 18:49:01 -0300

    Added usage of context validator in the async executor

--- juniper/src/lib.rs
+++ juniper/src/lib.rs
@@ -250,6 +250,16 @@ where
 {
     let document = parse_document_source(document_source, &root_node.schema)?;
 
+    {
+        let mut ctx = ValidatorContext::new(&root_node.schema, &document);
+        visit_all_rules(&mut ctx, &document);
+
+        let errors = ctx.into_errors();
+        if !errors.is_empty() {
+            return Err(GraphQLError::ValidationError(errors));
+        }
+    }
+
     let operation = get_operation(&document, operation_name)?;
 
     {
