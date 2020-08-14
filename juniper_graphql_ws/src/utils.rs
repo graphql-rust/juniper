513bd1cc9a86f26d0bc9d7216fa8a633d0ad95fb
LightDiscord <root@arnaud.sh>
2020-08-14 12:29:26 +0200

    fix(juniper_graphql_ws): correct null deserialization issue
    Closes #735

---
+++ juniper_graphql_ws/src/utils.rs
@@ -0,0 +1,9 @@
+use serde::{Deserialize, Deserializer};
+
+pub(crate) fn default_for_null<'de, D, T>(deserializer: D) -> Result<T, D::Error>
+where
+    D: Deserializer<'de>,
+    T: Deserialize<'de> + Default,
+{
+    Ok(Option::<T>::deserialize(deserializer)?.unwrap_or_default())
+}
