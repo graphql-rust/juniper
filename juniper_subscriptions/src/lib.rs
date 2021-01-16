5056a3c58f9abd577714a3d6008382dab2448e1f
Christian Legnitto <christian.legnitto@robinhood.com>
2021-01-15 19:52:18 -1000

    Release juniper_subscriptions 0.15.2

--- juniper_subscriptions/src/lib.rs
+++ juniper_subscriptions/src/lib.rs
@@ -9,7 +9,7 @@
 
 #![deny(missing_docs)]
 #![deny(warnings)]
-#![doc(html_root_url = "https://docs.rs/juniper_subscriptions/0.15.1")]
+#![doc(html_root_url = "https://docs.rs/juniper_subscriptions/0.15.2")]
 
 use std::{
     iter::FromIterator,
