f984237e4c172574cf2e5e7ed1934beb80214d27
Jerel Unruh <jerel.unruh@toyotaconnected.com>
2021-02-12 20:11:35 -0600

    Fix fmt warning for visit_all_rules

--- juniper/src/validation/mod.rs
+++ juniper/src/validation/mod.rs
@@ -10,11 +10,11 @@ mod visitor;
 #[cfg(test)]
 pub(crate) mod test_harness;
 
-pub use self::rules::visit_all_rules;
 pub use self::{
     context::{RuleError, ValidatorContext},
     input_value::validate_input_values,
     multi_visitor::MultiVisitorNil,
+    rules::visit_all_rules,
     traits::Visitor,
     visitor::visit,
 };
