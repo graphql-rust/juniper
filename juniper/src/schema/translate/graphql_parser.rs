e9fcb027bae881e71f9629aca6af0b31711781d9
ilslv <ilya.solovyiov@gmail.com>
2021-11-29 13:53:10 +0300

    Fix and add TODO to be resolved with GraphQL October 2021 Edition support

--- juniper/src/schema/translate/graphql_parser.rs
+++ juniper/src/schema/translate/graphql_parser.rs
@@ -190,6 +190,8 @@ impl GraphQLParserTranslator {
                 position: Pos::default(),
                 description: x.description.as_ref().map(|s| From::from(s.as_str())),
                 name: From::from(x.name.as_ref()),
+                // TODO: Support this with GraphQL October 2021 Edition
+                implements_interfaces: vec![],
                 directives: vec![],
                 fields: x
                     .fields
