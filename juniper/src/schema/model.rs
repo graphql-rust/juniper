eb8bbbef42bb66671ee0d02a06feac77eaef5cdc
ilslv <ilya.solovyiov@gmail.com>
2022-02-23 11:07:23 +0300

    Fix fragment spread tests

--- juniper/src/schema/model.rs
+++ juniper/src/schema/model.rs
@@ -415,10 +415,6 @@ impl<'a, S> SchemaType<'a, S> {
                     MetaType::Object(ObjectMeta {
                         ref interface_names,
                         ..
-                    })
-                    | MetaType::Interface(InterfaceMeta {
-                        ref interface_names,
-                        ..
                     }) => interface_names.iter().any(|iname| iname == name),
                     _ => false,
                 })
