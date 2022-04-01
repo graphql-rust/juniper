d3c9ce03dfc668b785027df1128d20fa9f3279f1
ilslv <ilya.solovyiov@gmail.com>
2022-04-01 15:11:44 +0300

    WIP [skip ci]

--- integration_tests/juniper_tests/src/codegen/derive_input_object.rs
+++ integration_tests/juniper_tests/src/codegen/derive_input_object.rs
@@ -1,8 +1,133 @@
 use fnv::FnvHashMap;
 use juniper::{
-    graphql_input_value, marker, DefaultScalarValue, FieldError, FromInputValue,
-    GraphQLInputObject, GraphQLType, GraphQLValue, InputValue, Registry, ToInputValue,
+    execute, graphql_input_value, graphql_object, graphql_value, graphql_vars, marker,
+    DefaultScalarValue, FieldError, FromInputValue, GraphQLInputObject, GraphQLType, GraphQLValue,
+    InputValue, Registry, ToInputValue,
 };
+
+use crate::util::{schema, schema_with_scalar};
+
+mod trivial {
+    use super::*;
+
+    #[derive(GraphQLInputObject)]
+    struct Point2D {
+        x: f64,
+        y: f64,
+    }
+
+    struct QueryRoot;
+
+    #[graphql_object]
+    impl QueryRoot {
+        fn x(point: Point2D) -> f64 {
+            point.x
+        }
+    }
+
+    #[tokio::test]
+    async fn resolves() {
+        const DOC: &str = r#"{
+            x(point: { x: 10, y: 20 })
+        }"#;
+
+        let schema = schema(QueryRoot);
+
+        assert_eq!(
+            execute(DOC, None, &schema, &graphql_vars! {}, &()).await,
+            Ok((graphql_value!({"x": 10.0}), vec![])),
+        );
+    }
+
+    #[tokio::test]
+    async fn is_graphql_input_object() {
+        const DOC: &str = r#"{
+            __type(name: "Point2D") {
+                kind
+            }
+        }"#;
+
+        let schema = schema(QueryRoot);
+
+        assert_eq!(
+            execute(DOC, None, &schema, &graphql_vars! {}, &()).await,
+            Ok((graphql_value!({"__type": {"kind": "INPUT_OBJECT"}}), vec![])),
+        );
+    }
+
+    #[tokio::test]
+    async fn uses_type_name() {
+        const DOC: &str = r#"{
+            __type(name: "Point2D") {
+                name
+            }
+        }"#;
+
+        let schema = schema(QueryRoot);
+
+        assert_eq!(
+            execute(DOC, None, &schema, &graphql_vars! {}, &()).await,
+            Ok((graphql_value!({"__type": {"name": "Point2D"}}), vec![])),
+        );
+    }
+
+    #[tokio::test]
+    async fn has_no_description() {
+        const DOC: &str = r#"{
+            __type(name: "Point2D") {
+                description
+            }
+        }"#;
+
+        let schema = schema(QueryRoot);
+
+        assert_eq!(
+            execute(DOC, None, &schema, &graphql_vars! {}, &()).await,
+            Ok((graphql_value!({"__type": {"description": null}}), vec![])),
+        );
+    }
+
+    #[tokio::test]
+    async fn has_input_fields() {
+        const DOC: &str = r#"{
+            __type(name: "Point2D") {
+                inputFields {
+                    name
+                    description
+                    type {
+                        ofType {
+                            name
+                        }
+                    }
+                    defaultValue
+                }
+            }
+        }"#;
+
+        let schema = schema(QueryRoot);
+
+        assert_eq!(
+            execute(DOC, None, &schema, &graphql_vars! {}, &()).await,
+            Ok((
+                graphql_value!({"__type": {"inputFields": [
+                    {
+                        "name": "x",
+                        "description": null,
+                        "type": {"ofType": {"name": "Float"}},
+                        "defaultValue": null,
+                    },
+                    {
+                        "name": "y",
+                        "description": null,
+                        "type": {"ofType": {"name": "Float"}},
+                        "defaultValue": null,
+                    },
+                ]}}),
+                vec![],
+            )),
+        );
+    }
+}
 
 #[derive(GraphQLInputObject, Debug, PartialEq)]
 #[graphql(
