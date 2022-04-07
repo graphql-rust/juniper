3f180db9c750a749686de1f3a69583cb0a34c8a4
tyranron <tyranron@gmail.com>
2022-04-07 11:58:18 +0300

    Reorganize `juniper_actix` crate

--- juniper_actix/src/lib.rs
+++ juniper_actix/src/lib.rs
@@ -1,41 +1,4 @@
-/*!
-
-# juniper_actix
-
-This repository contains the [actix][actix] web server integration for
-[Juniper][Juniper], a [GraphQL][GraphQL] implementation for Rust, its inspired and some parts are copied from [juniper_warp][juniper_warp]
-
-## Documentation
-
-For documentation, including guides and examples, check out [Juniper][Juniper].
-
-A basic usage example can also be found in the [API documentation][documentation].
-
-## Examples
-
-Check [examples/actix_server][example] for example code of a working actix
-server with GraphQL handlers.
-
-## Links
-
-* [Juniper][Juniper]
-* [API Reference][documentation]
-* [actix][actix]
-
-## License
-
-This project is under the BSD-2 license.
-
-Check the LICENSE file for details.
-
-[actix]: https://github.com/actix/actix-web
-[Juniper]: https://github.com/graphql-rust/juniper
-[GraphQL]: http://graphql.org
-[documentation]: https://docs.rs/juniper_actix
-[example]: https://github.com/graphql-rust/juniper/blob/master/juniper_actix/examples/actix_server.rs
-[juniper_warp]: https://github.com/graphql-rust/juniper/juniper_warp
-*/
-
+#![doc = include_str!("../README.md")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![deny(missing_docs)]
 #![deny(warnings)]
