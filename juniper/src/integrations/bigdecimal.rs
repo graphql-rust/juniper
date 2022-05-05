0316b9adbdb5f9c06c56d310722b9f27665668c3
tyranron <tyranron@gmail.com>
2022-05-05 19:52:08 +0300

    Correct `bigdecimal` integration

--- juniper/src/integrations/bigdecimal.rs
+++ juniper/src/integrations/bigdecimal.rs
@@ -1,9 +1,38 @@
+//! GraphQL support for [`bigdecimal`] crate types.
+//!
+//! # Supported types
+//!
+//! | Rust type      | GraphQL scalar |
+//! |----------------|----------------|
+//! | [`BigDecimal`] | `BigDecimal`   |
+//!
+//! [`BigDecimal`]: bigdecimal::BigDecimal
+
+use std::str::FromStr as _;
+
 use crate::{graphql_scalar, InputValue, ScalarValue, Value};
 
-#[graphql_scalar(with = bigdecimal_scalar, parse_token(String))]
-type BigDecimal = ::bigdecimal::BigDecimal;
-use std::str::FromStr;
-pub mod bigdecimal_scalar {
+/// Big decimal type.
+///
+/// Allows storing any real number to arbitrary precision; which avoids common
+/// floating point errors (such as 0.1 + 0.2 ≠ 0.3) at the cost of complexity.
+///
+/// Always serializes as `String`. But may be deserialized from `Int` and
+/// `Float` values too. It's not recommended to deserialize from a `Float`
+/// directly, as the floating point representation may be unexpected.
+///
+/// See also [`bigdecimal`] crate for details.
+///
+/// [`bigdecimal`]: https://docs.rs/bigdecimal
+#[graphql_scalar(
+    with = bigdecimal_scalar,
+    parse_token(i32, f64, String),
+    specified_by_url = "https://docs.rs/bigdecimal",
+)]
+type BigDecimal = bigdecimal::BigDecimal;
+
+mod bigdecimal_scalar {
+    use std::convert::TryFrom as _;
 
     use super::*;
 
@@ -12,28 +41,92 @@ pub mod bigdecimal_scalar {
     }
 
     pub(super) fn from_input<S: ScalarValue>(v: &InputValue<S>) -> Result<BigDecimal, String> {
-        v.as_string_value()
-            .ok_or_else(|| format!("Expected `String`, found: {}", v))
-            .and_then(|s| {
-                BigDecimal::from_str(s).map_err(|e| format!("Failed to parse `BigDecimal`: {}", e))
-            })
+        if let Some(i) = v.as_int_value() {
+            Ok(BigDecimal::from(i))
+        } else if let Some(f) = v.as_float_value() {
+            BigDecimal::try_from(f)
+                .map_err(|e| format!("Failed to parse `BigDecimal` from `Float`: {}", e))
+        } else {
+            v.as_string_value()
+                .ok_or_else(|| format!("Expected `String`, found: {}", v))
+                .and_then(|s| {
+                    BigDecimal::from_str(s)
+                        .map_err(|e| format!("Failed to parse `BigDecimal` from `String`: {}", e))
+                })
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::str::FromStr as _;
+
+    use crate::{graphql_input_value, FromInputValue as _, InputValue, ToInputValue as _};
+
+    use super::BigDecimal;
+
+    #[test]
+    fn parses_correct_input() {
+        for (input, expected) in [
+            (graphql_input_value!("4.20"), "4.20"),
+            (graphql_input_value!("0"), "0"),
+            (
+                graphql_input_value!("999999999999.999999999"),
+                "999999999999.999999999",
+            ),
+            (
+                graphql_input_value!("87553378877997984345"),
+                "87553378877997984345",
+            ),
+            (graphql_input_value!(123), "123"),
+            (graphql_input_value!(0), "0"),
+            (graphql_input_value!(43.44), "43.44"),
+        ] {
+            let input: InputValue = input;
+            let parsed = BigDecimal::from_input_value(&input);
+            let expected = BigDecimal::from_str(expected).unwrap();
+
+            assert!(
+                parsed.is_ok(),
+                "failed to parse `{:?}`: {:?}",
+                input,
+                parsed.unwrap_err(),
+            );
+            assert_eq!(parsed.unwrap(), expected, "input: {:?}", input);
+        }
     }
 
-    #[cfg(test)]
-    mod test {
-        use super::*;
+    #[test]
+    fn fails_on_invalid_input() {
+        for input in [
+            graphql_input_value!(""),
+            graphql_input_value!("0,0"),
+            graphql_input_value!("12,"),
+            graphql_input_value!("1996-12-19T14:23:43"),
+            graphql_input_value!("i'm not even a number"),
+            graphql_input_value!(null),
+            graphql_input_value!(false),
+        ] {
+            let input: InputValue = input;
+            let parsed = BigDecimal::from_input_value(&input);
 
-        use crate::{graphql_input_value, FromInputValue, InputValue};
+            assert!(parsed.is_err(), "allows input: {:?}", input);
+        }
+    }
 
-        #[test]
-        fn bigdecimal_from_input() {
-            let raw = "4.20";
-            let input: InputValue = graphql_input_value!((raw));
+    #[test]
+    fn formats_correctly() {
+        for raw in [
+            "4.20",
+            "0",
+            "999999999999.999999999",
+            "87553378877997984345",
+            "123",
+            "43.44",
+        ] {
+            let actual: InputValue = BigDecimal::from_str(raw).unwrap().to_input_value();
 
-            let parsed: BigDecimal = FromInputValue::from_input_value(&input).unwrap();
-            let id = BigDecimal::from_str(raw).unwrap();
-
-            assert_eq!(parsed, id);
+            assert_eq!(actual, graphql_input_value!((raw)), "on value: {}", raw);
         }
     }
 }
