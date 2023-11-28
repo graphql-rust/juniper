474c54ffebe141fdbcb421fb76c2b75db4c16192
tyranron <tyranron@gmail.com>
2023-11-28 15:59:02 +0100

    Add integration test, vol.2

--- tests/integration/tests/disabled_introspection.rs
+++ tests/integration/tests/disabled_introspection.rs
@@ -2,8 +2,8 @@
 
 use futures::stream;
 use juniper::{
-    execute, graphql_object, graphql_subscription, graphql_vars,
-    resolve_into_stream, GraphQLError, RootNode,
+    execute, graphql_object, graphql_subscription, graphql_vars, resolve_into_stream, GraphQLError,
+    RootNode,
 };
 
 pub struct Query;
@@ -34,8 +34,109 @@ impl Subscription {
 }
 
 #[tokio::test]
-async fn explicit_mutation_type() {
-    let query = r#"mutation { __type }"#;
+async fn query_schema() {
+    // language=GraphQL
+    let query = "query { __schema { queryType { name } } }";
+
+    let schema = RootNode::new(Query, Mutation, Subscription).disable_introspection();
+
+    match execute(query, None, &schema, &graphql_vars! {}, &()).await {
+        Err(GraphQLError::ValidationError(errors)) => {
+            assert_eq!(errors.len(), 1);
+
+            let err = errors.first().unwrap();
+
+            assert_eq!(
+                err.message(),
+                "GraphQL introspection is not allowed, but the operation contained `__schema`",
+            );
+            assert_eq!(err.locations()[0].index(), 8);
+            assert_eq!(err.locations()[0].line(), 0);
+            assert_eq!(err.locations()[0].column(), 8);
+        }
+        res => panic!("expected `ValidationError`, returned: {res:#?}"),
+    }
+}
+
+#[tokio::test]
+async fn mutation_schema() {
+    // language=GraphQL
+    let query = "mutation { __schema { queryType { name } } }";
+
+    let schema = RootNode::new(Query, Mutation, Subscription).disable_introspection();
+
+    match execute(query, None, &schema, &graphql_vars! {}, &()).await {
+        Err(GraphQLError::ValidationError(errors)) => {
+            assert_eq!(errors.len(), 2);
+
+            let err = errors.first().unwrap();
+
+            assert_eq!(
+                err.message(),
+                "GraphQL introspection is not allowed, but the operation contained `__schema`",
+            );
+            assert_eq!(err.locations()[0].index(), 11);
+            assert_eq!(err.locations()[0].line(), 0);
+            assert_eq!(err.locations()[0].column(), 11);
+        }
+        res => panic!("expected `ValidationError`, returned: {res:#?}"),
+    }
+}
+
+#[tokio::test]
+async fn subscription_schema() {
+    // language=GraphQL
+    let query = "subscription { __schema { queryType { name } } }";
+
+    let schema = RootNode::new(Query, Mutation, Subscription).disable_introspection();
+
+    match resolve_into_stream(query, None, &schema, &graphql_vars! {}, &()).await {
+        Err(GraphQLError::ValidationError(errors)) => {
+            assert_eq!(errors.len(), 2);
+
+            let err = errors.first().unwrap();
+
+            assert_eq!(
+                err.message(),
+                "GraphQL introspection is not allowed, but the operation contained `__schema`",
+            );
+            assert_eq!(err.locations()[0].index(), 15);
+            assert_eq!(err.locations()[0].line(), 0);
+            assert_eq!(err.locations()[0].column(), 15);
+        }
+        _ => panic!("expected `ValidationError`"),
+    };
+}
+
+#[tokio::test]
+async fn query_type() {
+    // language=GraphQL
+    let query = r#"query { __type(name: "String") { name } }"#;
+
+    let schema = RootNode::new(Query, Mutation, Subscription).disable_introspection();
+
+    match execute(query, None, &schema, &graphql_vars! {}, &()).await {
+        Err(GraphQLError::ValidationError(errors)) => {
+            assert_eq!(errors.len(), 1);
+
+            let err = errors.first().unwrap();
+
+            assert_eq!(
+                err.message(),
+                "GraphQL introspection is not allowed, but the operation contained `__type`",
+            );
+            assert_eq!(err.locations()[0].index(), 8);
+            assert_eq!(err.locations()[0].line(), 0);
+            assert_eq!(err.locations()[0].column(), 8);
+        }
+        res => panic!("expected `ValidationError`, returned: {res:#?}"),
+    }
+}
+
+#[tokio::test]
+async fn mutation_type() {
+    // language=GraphQL
+    let query = r#"mutation { __type(name: "String") { name } }"#;
 
     let schema = RootNode::new(Query, Mutation, Subscription).disable_introspection();
 
@@ -58,8 +159,9 @@ async fn explicit_mutation_type() {
 }
 
 #[tokio::test]
-async fn explicit_subscription_type() {
-    let query = r#"subscription Subscription { __type }"#;
+async fn subscription_type() {
+    // language=GraphQL
+    let query = r#"subscription Subscription { __type(name: "String") { name } }"#;
 
     let schema = RootNode::new(Query, Mutation, Subscription).disable_introspection();
 
