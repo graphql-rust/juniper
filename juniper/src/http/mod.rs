2e7eece11eafa4bfba39a0c63e2be7d5dde2ccdb
tyranron <tyranron@gmail.com>
2023-11-28 17:14:36 +0100

    Impl `PartialEq` for `GraphQLResponse`

--- juniper/src/http/mod.rs
+++ juniper/src/http/mod.rs
@@ -163,7 +163,7 @@ where
 /// This struct implements Serialize, so you can simply serialize this
 /// to JSON and send it over the wire. Use the `is_ok` method to determine
 /// whether to send a 200 or 400 HTTP status code.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct GraphQLResponse<S = DefaultScalarValue>(
     Result<(Value<S>, Vec<ExecutionError<S>>), GraphQLError>,
 );
