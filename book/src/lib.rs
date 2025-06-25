a3754b524a84979535c0faca894113d75e4f63f0
tyranron <tyranron@gmail.com>
2025-06-25 19:17:06 +0300

    Use `juniper_book` crate only for Book dependencies

--- book/src/lib.rs
+++ book/src/lib.rs
@@ -1,3 +1,9 @@
 //! Crate keeping dependencies for running Book tests.
 
+use anyhow as _;
 use dataloader as _;
+use derive_more as _;
+use juniper as _;
+use juniper_subscriptions as _;
+use serde_json as _;
+use tokio as _;
