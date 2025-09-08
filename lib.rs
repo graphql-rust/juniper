0a9e99331e03564a520bfe372d69b18f55912fe3
tyranron <tyranron@users.noreply.github.com>
2025-09-08 19:23:56 +0000

    deploy: ee96ed113963c0d7406037261fde0e347e219ea6

---
+++ lib.rs
@@ -0,0 +1,10 @@
+//! Crate keeping dependencies for running Book tests.
+
+use anyhow as _;
+use dataloader as _;
+use derive_more as _;
+use jiff as _;
+use juniper as _;
+use juniper_subscriptions as _;
+use serde_json as _;
+use tokio as _;
